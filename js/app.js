714e21c40d705be084040ab8ee9ef4dbf1b0e9d2
Kevin Caicedo <ing.sys.kevincaicedo@gmail.com>
2018-08-14 19:15:21 -0500

    fixed bug update and add edit remove todo

--- js/app.js
+++ js/app.js
@@ -1,34 +1,60 @@
 var app = new Vue({
 	el: '#todo-app',
 	mounted: function(){
-
-	},
-	computed: {
-
+		this.showsTodoList()
 	},
 	data: {
 		lists: [],
-		tempId: 0
+		cacheTemp: ''
 	},
 	methods: {
 		newTask(data){
 			// append new item todo list
-			const _task = data.target.elements.task.value
-			if(_task.trim().length == 0)
+			const _task = data.target.elements.task.value.trim()
+			data.target.elements.task.value = ""
+			if(_task.length == 0)
 				return
-			this.lists.push({ id: this.tempId++, name: _task, finished: false })
+			var item = { name: _task, finished: false }
+			
+			// resquest post new todo
+			axios.post("https://protected-plains-91489.herokuapp.com/api/todos", item).then((res) => {
+				M.toast({html: res.data.info})
+				item['id'] = res.data.data
+				this.lists.push(item)
+            }).catch((error) => {
+            	M.toast({html: 'Error al guardar tu tarea!'})
+            	this.lists.pop()
+            });
+
 		},
-		finishedTask(todo){
-			console.log(todo)
+		preventUpdate(name){
+			this.cacheTemp = name.trim();
 		},
-		editTaskTodo(todo){
-			console.log(todo)
+		editTaskTodo(todo, message=null){
+			if(!message && todo.name.trim() === this.cacheTemp)
+				return
+			axios.put("https://protected-plains-91489.herokuapp.com/api/todos/"+todo.id, todo).then((res) => {
+				M.toast({html: message ? ( todo.finished ? message : 'Actividad restablecida' ) : res.data.info })
+            }).catch((error) => {
+            	M.toast({html: 'Error al guardar tu tarea!'})
+            });
 		},
-		removeTodo(todo){
-			console.log(todo)
+		removeTodo(id, index){
+			var item = this.lists.splice( index, 1)
+			axios.delete("https://protected-plains-91489.herokuapp.com/api/todos/"+id).then((res) => {
+				M.toast({html: res.data.info})
+            }).catch((error) => {
+            	M.toast({html: 'Error al guardar tu tarea!'})
+            	this.lists.splice( index, 1, item)
+            });
 		},
 		showsTodoList(){
-
+			axios.get("https://protected-plains-91489.herokuapp.com/api/todos").then((res) => {
+				this.lists = res.data.data
+				console.log(this.lists)
+            }).catch((error) => {
+            	M.toast({html: 'Error al carga tus tareas!'})
+            });
 		}
 	}
 })
